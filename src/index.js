075d5c4427a57a851d75faf540a01b91621c9e9e
seyha30 <sinseyha1x@gmail.com>
2023-03-13 22:22:09 +0700

    ccc

--- src/index.js
+++ src/index.js
@@ -2,4 +2,60 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello</h1>);
+
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>It is {props.date.toLocaleTimeString()}</h2>
+    </div>
+  );
+}
+class Clocks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Hello , world</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+function tick() {
+  const elements = (
+    <div>
+      <h1>Hello, world111</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+
+      <Clock date={new Date()} />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Clocks />
+      <Clocks />
+      <Clocks />
+    </div>
+  );
+}
+root.render(<App />);
+setInterval(tick, 1000);
